80340ce19216a695247e07c0d121424ad51ace5d
bastionkid <akash.mercer@gmail.com>
2022-09-15 18:49:28 +0530

    `core` modules added

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("android.library")
+}
+
+android {
+    namespace = "com.azuredragon.core.data"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
