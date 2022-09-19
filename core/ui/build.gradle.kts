5caeae32dcb0dc821662d64683803415e4156e14
bastionkid <akash.mercer@gmail.com>
2022-09-19 18:29:51 +0530

    compose dependencies added in `core:ui` module

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("android.library")
+    id("android.library.compose")
 }
 
 android {
@@ -17,5 +18,28 @@ android {
 }
 
 dependencies {
-    testImplementation(libs.junit4)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.constraintlayout.compose)
+
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.ui.util)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.accompanist.flowlayout)
+
+    implementation(libs.coil.kt.compose)
+
+    androidTestImplementation(libs.junit4)
 }
