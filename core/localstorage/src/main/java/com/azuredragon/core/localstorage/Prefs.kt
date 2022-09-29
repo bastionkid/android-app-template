f79925c4661938b6fb24784a79660291654bf396
bastionkid <akash.mercer@gmail.com>
2022-09-29 17:27:17 +0530

    Prefs updated

--- core/localstorage/src/main/java/com/azuredragon/core/localstorage/Prefs.kt
+++ core/localstorage/src/main/java/com/azuredragon/core/localstorage/Prefs.kt
@@ -15,4 +15,30 @@ interface Prefs {
     fun getString(key: String, default: String? = null): Flow<String?>
 
     fun getStringSet(key: String, default: Set<String>? = null): Flow<Set<String>?>
+
+    suspend fun putInt(key: String, value: Int)
+
+    suspend fun putLong(key: String, value: Long)
+
+    suspend fun putFloat(key: String, value: Float)
+
+    suspend fun putBoolean(key: String, value: Boolean)
+
+    suspend fun putString(key: String, value: String)
+
+    suspend fun putStringSet(key: String, value: Set<String>)
+
+    suspend fun removeInt(key: String)
+
+    suspend fun removeLong(key: String)
+
+    suspend fun removeFloat(key: String)
+
+    suspend fun removeBoolean(key: String)
+
+    suspend fun removeString(key: String)
+
+    suspend fun removeStringSet(key: String)
+
+    suspend fun clearAll()
 }
--- core/localstorage/src/main/java/com/azuredragon/core/localstorage/PrefsImpl.kt
+++ core/localstorage/src/main/java/com/azuredragon/core/localstorage/PrefsImpl.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.longPreferencesKey
@@ -39,5 +40,84 @@ class PrefsImpl(private val context: Context): Prefs {
 
     override fun getStringSet(key: String, default: Set<String>?): Flow<Set<String>?> {
         return context.dataStore.data.map { it[stringSetPreferencesKey(key)] ?: default }
+    }
+
+    override suspend fun putInt(key: String, value: Int) {
+        context.dataStore.edit { preferences -> preferences.clear()  }
+        context.dataStore.edit { preferences ->
+            preferences[intPreferencesKey(key)] = value
+        }
+    }
+
+    override suspend fun putLong(key: String, value: Long) {
+        context.dataStore.edit { preferences ->
+            preferences[longPreferencesKey(key)] = value
+        }
+    }
+
+    override suspend fun putFloat(key: String, value: Float) {
+        context.dataStore.edit { preferences ->
+            preferences[floatPreferencesKey(key)] = value
+        }
+    }
+
+    override suspend fun putBoolean(key: String, value: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[booleanPreferencesKey(key)] = value
+        }
+    }
+
+    override suspend fun putString(key: String, value: String) {
+        context.dataStore.edit { preferences ->
+            preferences[stringPreferencesKey(key)] = value
+        }
+    }
+
+    override suspend fun putStringSet(key: String, value: Set<String>) {
+        context.dataStore.edit { preferences ->
+            preferences[stringSetPreferencesKey(key)] = value
+        }
+    }
+
+    override suspend fun removeInt(key: String) {
+        context.dataStore.edit { preferences ->
+            preferences.remove(intPreferencesKey(key))
+        }
+    }
+
+    override suspend fun removeLong(key: String) {
+        context.dataStore.edit { preferences ->
+            preferences.remove(longPreferencesKey(key))
+        }
+    }
+
+    override suspend fun removeFloat(key: String) {
+        context.dataStore.edit { preferences ->
+            preferences.remove(floatPreferencesKey(key))
+        }
+    }
+
+    override suspend fun removeBoolean(key: String) {
+        context.dataStore.edit { preferences ->
+            preferences.remove(booleanPreferencesKey(key))
+        }
+    }
+
+    override suspend fun removeString(key: String) {
+        context.dataStore.edit { preferences ->
+            preferences.remove(stringPreferencesKey(key))
+        }
+    }
+
+    override suspend fun removeStringSet(key: String) {
+        context.dataStore.edit { preferences ->
+            preferences.remove(stringSetPreferencesKey(key))
+        }
+    }
+
+    override suspend fun clearAll() {
+        context.dataStore.edit { preferences ->
+            preferences.clear()
+        }
     }
 }
