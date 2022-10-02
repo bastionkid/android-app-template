17b535d9cbeec3c7051449327656aca70f763f0a
bastionkid <akash.mercer@gmail.com>
2022-10-02 16:38:03 +0530

    Text added to both Login & Home Frgaments

--- feature/home/presentation/src/main/java/com/azuredragon/home/presentation/HomeFragment.kt
+++ feature/home/presentation/src/main/java/com/azuredragon/home/presentation/HomeFragment.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
 import com.azuredragon.core.ui.compose.composeWithSurface
 
@@ -15,7 +21,12 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         return composeWithSurface(requireContext()) {
-
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(
+                    text = stringResource(id = R.string.label_home),
+                    modifier = Modifier.align(Alignment.Center),
+                )
+            }
         }
     }
 }
