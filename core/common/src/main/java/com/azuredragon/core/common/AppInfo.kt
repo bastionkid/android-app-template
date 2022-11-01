d3464cf32b3f3c3d949f971e19cdefac94f5801b
bastionkid <akash.mercer@gmail.com>
2022-11-01 11:17:20 +0530

    default value for flavor added

--- core/common/src/main/java/com/azuredragon/core/common/AppInfo.kt
+++ core/common/src/main/java/com/azuredragon/core/common/AppInfo.kt
@@ -3,6 +3,6 @@ package com.azuredragon.core.common
 class AppInfo(
     val versionName: String,
     val versionCode: String,
-    val flavor: String,
+    val flavor: String = "",
     val deviceInfo: String,
 )
