57a181ee796578561c67d34cbe02d9bc0f5c2f5d
bastionkid <akash.mercer@gmail.com>
2022-11-01 12:07:04 +0530

    `ShowLoadingAnimation()` updated & `ShowLottieAnimation()` added

--- core/ui/src/main/java/com/azuredragon/core/ui/compose/LottieAnimation.kt
+++ core/ui/src/main/java/com/azuredragon/core/ui/compose/LottieAnimation.kt
@@ -1,23 +1,38 @@
 package com.azuredragon.core.ui.compose
 
+import androidx.annotation.RawRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.azuredragon.core.ui.R
 
 @Composable
-fun ShowLoadingAnimation(modifier: Modifier = Modifier.fillMaxSize()) {
+fun ShowLoadingAnimation(modifier: Modifier = Modifier) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
-    val progress by animateLottieCompositionAsState(composition)
 
     LottieAnimation(
         composition = composition,
-        progress = { progress },
+        restartOnPlay = true,
+        iterations = Int.MAX_VALUE,
+        modifier = modifier.fillMaxSize(),
+    )
+}
+
+@Composable
+fun ShowLottieAnimation(
+    @RawRes lottieRes: Int,
+    modifier: Modifier = Modifier,
+) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieRes))
+
+    LottieAnimation(
+        composition = composition,
+        restartOnPlay = true,
+        iterations = Int.MAX_VALUE,
         modifier = modifier,
     )
 }
