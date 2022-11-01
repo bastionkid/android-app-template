0a4a4fac75bd492e566017e4f57a3564f3514367
bastionkid <akash.mercer@gmail.com>
2022-11-01 11:16:57 +0530

    `encodeDefaults` added to commonJson to ensure properties with default values are present in serialized json

--- core/common/src/main/java/com/azuredragon/core/common/Json.kt
+++ core/common/src/main/java/com/azuredragon/core/common/Json.kt
@@ -2,12 +2,13 @@ package com.azuredragon.core.common
 
 import kotlinx.serialization.json.Json
 
-val json: Json = Json {
+val commonJson: Json = Json {
     // ignoreUnknownKeys allows unknown properties in the input JSON to be ignored
     ignoreUnknownKeys = true
     //  This was required to fix this issue https://github.com/Kotlin/kotlinx.serialization/issues/1450#issuecomment-841214332
     useAlternativeNames = false
     // Reference: https://blog.jetbrains.com/kotlin/2021/09/kotlinx-serialization-1-3-released/#excluding-nulls
     explicitNulls = false
+    encodeDefaults = true
     useArrayPolymorphism = true
 }
