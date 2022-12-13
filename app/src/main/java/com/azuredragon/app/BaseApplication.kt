0c85680d39fc8c3c43e542a5b36390c973f18279
bastionkid <akash.mercer@gmail.com>
2022-12-13 16:15:45 +0700

    `koin` initialization block added

--- app/src/main/java/com/azuredragon/app/BaseApplication.kt
+++ app/src/main/java/com/azuredragon/app/BaseApplication.kt
@@ -2,6 +2,8 @@ package com.azuredragon.app
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class BaseApplication: Application() {
 
@@ -9,5 +11,13 @@ class BaseApplication: Application() {
         super.onCreate()
 
         FirebaseApp.initializeApp(this)
+
+        startKoin {
+            androidContext(this@BaseApplication)
+
+            val isDebug = BuildConfig.DEBUG
+
+            modules(emptyList())
+        }
     }
 }
