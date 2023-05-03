184a26651357fed1cccc8fe2e97e70e31a407947
bastionkid <akash.mercer@gmail.com>
2023-05-03 13:24:14 +0530

    changes to resolve 'Use a ( subshell ) to avoid having to cd back' inspection warning

---
+++ .github/scripts/vector_optimizer.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# navigate to the root directory
+cd ../..
+
+echo "root directory: $(pwd)"
+
+# navigate to the app modules' res directory
+cd app/src/main/res/
+
+# navigate to the drawable directories and print the current directory path
+for dir in $(find . -type d -name "drawable*"); do
+  cd "$dir"
+  echo "drawable directory: $(pwd)"
+  cd ./..
+done
+
+# navigate to the root directory
+cd ../../../..
+
+echo "root directory: $(pwd)"
+
+# navigate to the core-ui modules' res directory
+cd core/ui/src/main/res/
+
+# navigate to the drawable directories and print the current directory path
+for dir in $(find . -type d -name "drawable*"); do
+  cd "$dir"
+  echo "drawable directory: $(pwd)"
+  cd ./..
+done
+
+# navigate to the root directory
+cd ../../../../..
+
+echo "root directory: $(pwd)"
+
+# navigate to the feature directory having all the feature modules
+cd feature
+
+# iterate through all the modules present inside feature directory
+for dir in */; do
+  # navigate to the current core modules' res directory
+  cd "$dir"presentation/src/main/res/
+
+  echo "current directory: $(pwd)"
+
+  # navigate to the drawable directories and print the current directory path
+  for dir in $(find . -type d -name "drawable*"); do
+  	cd "$dir"
+  	echo "drawable directory: $(pwd)"
+  	cd ./..
+  done
+
+  cd ../../../../..
+done
