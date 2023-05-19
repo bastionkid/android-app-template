8f661e3731c0efc9a62219a79b69706da18d1d78
bastionkid <akash.mercer@gmail.com>
2023-05-19 18:42:20 +0530

    `html_head` commented out as it's supported by default in Github PR comments
    Signed-off-by: bastionkid <akash.mercer@gmail.com>

--- .github/scripts/compare_apks.py
+++ .github/scripts/compare_apks.py
@@ -2,151 +2,151 @@ import os
 import sys
 import zipfile
 
-html_head = """
-    <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link href="css/style.css" rel="stylesheet"><script defer="" src="js/script.js"></script><style>
-* {
-  font-family:
-    -apple-system,
-    "system-ui",
-    BlinkMacSystemFont,
-    "Segoe UI",
-    Helvetica,
-    Arial,
-    sans-serif,
-    "Apple Color Emoji",
-    "Segoe UI Emoji";
-}
-
-body {
-  padding: 15px;
-  font-size: 14px;
-  line-height: 1.5;
-  word-wrap: break-word;
-}
-
-h1, h2, h3, h4, h5 {
-  margin-top: 24px;
-  margin-bottom: 16px;
-  font-weight: 600;
-}
-
-h1 {
-  margin-top: 0px;
-  border-bottom: 1px solid rgb(216, 222, 228);
-  font-size: 2em;
-  padding-bottom: 0.3em;
-}
-
-h3 {
-  font-size: 1.25em;
-}
-
-h4 {
-  font-size: 1em;
-}
-
-h3 code, h4 code {
-  font-size: 100%;
-}
-
-a {
-  color: rgb(9, 105, 218);
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-
-b {
-  font-weight: 600;
-}
-
-code, pre {
-  font-family:
-    ui-monospace,
-    SFMono-Regular,
-    SF Mono,
-    Menlo,
-    Consolas,
-    Liberation Mono,
-    monospace;
-  font-size: 85%;
-  border-radius: 6px;
-}
-
-code {
-  background-color: rgba(175, 184, 193, 0.2);
-  padding: 2.38px 4.76px;
-}
-
-pre {
-  background-color: rgb(246, 248, 250);
-  padding: 16px;
-}
-
-table, td, th {
-  border-color: rgb(208, 215, 222);
-  border-width: 1px;
-  border-style: solid;
-  border-collapse: collapse;
-}
-
-td, th {
-  padding: 6px 13px;
-}
-
-tr:nth-child(even) {
-  background-color: rgb(234, 238, 242);
-}
-
-.white_table tr:nth-child(even) {
-  background-color: rgb(255, 255, 255);
-}
-
-sup>a::before {
-  content: "[";
-}
-
-sup>a::after {
-  content: "]";
-}
-
-.diff-plus, .diff-minus {
-  font-family:
-    ui-monospace,
-    SFMono-Regular,
-    SF Mono,
-    Menlo,
-    Consolas,
-    Liberation Mono,
-    monospace;
-}
-
-.diff-plus {
-  color: rgb(17, 99, 41);
-  background-color: rgb(218, 251, 225);
-}
-
-.diff-minus {
-  color: rgb(130, 7, 30);
-  background-color: rgb(255, 235, 233);
-}
-
-img {
-  width: 350px;
-}
-
-.perfetto {
-  color: rgb(9, 105, 218);
-}
-
-.perfetto:hover {
-  cursor: pointer;
-  text-decoration: underline;
-}
-</style></head>
-    """
+# html_head = """
+#     <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link href="css/style.css" rel="stylesheet"><script defer="" src="js/script.js"></script><style>
+# * {
+#   font-family:
+#     -apple-system,
+#     "system-ui",
+#     BlinkMacSystemFont,
+#     "Segoe UI",
+#     Helvetica,
+#     Arial,
+#     sans-serif,
+#     "Apple Color Emoji",
+#     "Segoe UI Emoji";
+# }
+#
+# body {
+#   padding: 15px;
+#   font-size: 14px;
+#   line-height: 1.5;
+#   word-wrap: break-word;
+# }
+#
+# h1, h2, h3, h4, h5 {
+#   margin-top: 24px;
+#   margin-bottom: 16px;
+#   font-weight: 600;
+# }
+#
+# h1 {
+#   margin-top: 0px;
+#   border-bottom: 1px solid rgb(216, 222, 228);
+#   font-size: 2em;
+#   padding-bottom: 0.3em;
+# }
+#
+# h3 {
+#   font-size: 1.25em;
+# }
+#
+# h4 {
+#   font-size: 1em;
+# }
+#
+# h3 code, h4 code {
+#   font-size: 100%;
+# }
+#
+# a {
+#   color: rgb(9, 105, 218);
+#   text-decoration: none;
+# }
+#
+# a:hover {
+#   text-decoration: underline;
+# }
+#
+# b {
+#   font-weight: 600;
+# }
+#
+# code, pre {
+#   font-family:
+#     ui-monospace,
+#     SFMono-Regular,
+#     SF Mono,
+#     Menlo,
+#     Consolas,
+#     Liberation Mono,
+#     monospace;
+#   font-size: 85%;
+#   border-radius: 6px;
+# }
+#
+# code {
+#   background-color: rgba(175, 184, 193, 0.2);
+#   padding: 2.38px 4.76px;
+# }
+#
+# pre {
+#   background-color: rgb(246, 248, 250);
+#   padding: 16px;
+# }
+#
+# table, td, th {
+#   border-color: rgb(208, 215, 222);
+#   border-width: 1px;
+#   border-style: solid;
+#   border-collapse: collapse;
+# }
+#
+# td, th {
+#   padding: 6px 13px;
+# }
+#
+# tr:nth-child(even) {
+#   background-color: rgb(234, 238, 242);
+# }
+#
+# .white_table tr:nth-child(even) {
+#   background-color: rgb(255, 255, 255);
+# }
+#
+# sup>a::before {
+#   content: "[";
+# }
+#
+# sup>a::after {
+#   content: "]";
+# }
+#
+# .diff-plus, .diff-minus {
+#   font-family:
+#     ui-monospace,
+#     SFMono-Regular,
+#     SF Mono,
+#     Menlo,
+#     Consolas,
+#     Liberation Mono,
+#     monospace;
+# }
+#
+# .diff-plus {
+#   color: rgb(17, 99, 41);
+#   background-color: rgb(218, 251, 225);
+# }
+#
+# .diff-minus {
+#   color: rgb(130, 7, 30);
+#   background-color: rgb(255, 235, 233);
+# }
+#
+# img {
+#   width: 350px;
+# }
+#
+# .perfetto {
+#   color: rgb(9, 105, 218);
+# }
+#
+# .perfetto:hover {
+#   cursor: pointer;
+#   text-decoration: underline;
+# }
+# </style></head>
+#     """
 
 # add/update the value (i.e. size) based on the present of key
 def update_if_present(components, key, value):
@@ -199,7 +199,7 @@ def format_size_with_indicator(size):
 # generate html file containing size diff in HRF
 def generate_size_diff_html(components1, components2, apk1Sha, apk2Sha, apk1Size, apk2Size):
     html = "<html>"
-    html += html_head
+    # html += html_head
     html += "<body><h1>Raw Size Report</h1><h3>Affected Products</h3>"
     html += "<ul><li><h4><code>apk</code></h4><table>"
     html += f"<tr><th>Component</th><th>Base ({apk1Sha})</th><th>Merge ({apk2Sha})</th><th>Diff</th></tr>"
