d952450bf539a24d7dcebd085ca7bb64d70a229b
bastionkid <akash.mercer@gmail.com>
2023-06-13 16:39:33 +0530

    dummy changes added
    Signed-off-by: bastionkid <akash.mercer@gmail.com>

--- feature/login/presentation/src/main/java/com/azuredragon/login/presentation/LoginFragment.kt
+++ feature/login/presentation/src/main/java/com/azuredragon/login/presentation/LoginFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
 import com.azuredragon.core.ui.compose.composeWithSurface
@@ -26,6 +28,8 @@ class LoginFragment : Fragment() {
 					text = stringResource(id = R.string.label_login),
 					modifier = Modifier.align(Alignment.Center),
 				)
+
+				Image(painter = painterResource(id = R.drawable.wallpaper), contentDescription = null)
 			}
 		}
 	}
