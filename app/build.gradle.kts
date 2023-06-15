cb2d0107d7ee320be381fb427d35ab45d81a6037
bastionkid <akash.mercer@gmail.com>
2023-06-15 11:16:35 +0530

    `libs.barcode` added
    Signed-off-by: bastionkid <akash.mercer@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -189,6 +189,8 @@ dependencies {
     implementation(libs.firebase.crashlytics.ndk)
     implementation(libs.firebase.performance)
 
+    implementation(libs.barcode)
+
     testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.test.espresso.core)
